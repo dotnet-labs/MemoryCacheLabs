5116e41d33e64d123e7f1dd024ef7cd4a7959a91
Changhui Xu <changhuixu@hotmail.com>
2021-01-26 12:17:22 -0600

    add a unit test for an API controller action method

---
+++ ImmutableCaches.UnitTests/DepartmentsControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using ImmutableCaches.Controllers;
+using ImmutableCaches.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ImmutableCaches.UnitTests
+{
+    [TestClass]
+    public class DepartmentsControllerTests
+    {
+        [TestMethod]
+        public async Task GetUserNameById_ShouldReturnCorrectResponse()
+        {
+            var record = new DepartmentNameCacheRecord(123, "Test 123");
+            var cachedDepartmentNamesMock = new Mock<ICachedDepartmentNames>();
+            cachedDepartmentNamesMock.Setup(x => x.GetDepartmentNameById(123))
+                .ReturnsAsync(record);
+
+            var controller = new DepartmentsController(new NullLogger<DepartmentsController>(),
+                cachedDepartmentNamesMock.Object);
+
+            var result = await controller.GetUserNameById(123);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+
+            var responseObj = (result as OkObjectResult)?.Value;
+            Assert.IsInstanceOfType(responseObj, typeof(DepartmentNameCacheRecord));
+            Assert.AreEqual(responseObj, record);
+        }
+    }
+}
